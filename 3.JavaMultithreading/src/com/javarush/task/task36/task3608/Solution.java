bc2a26ac2a305b406f9659bcbd0fe2c45bdc52cc
Densdix <densdix@mail.ru>
2018-11-29 16:06:24 +0200

    Complete MVC 5

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -16,5 +16,6 @@ public class Solution {
         controller.setModel(model);
         controller.setUsersView(usersView);
         usersView.fireEventShowAllUsers();
+        usersView.fireEventShowDeletedUsers();
     }
 }
