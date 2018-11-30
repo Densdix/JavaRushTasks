5254def4be1739efedb8f3ec9728de18c11956b4
Densdix <densdix@mail.ru>
2018-11-30 02:50:33 +0200

    task3608 has done

--- 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
+++ 3.JavaMultithreading/src/com/javarush/task/task36/task3608/Solution.java
@@ -22,6 +22,7 @@ public class Solution {
         usersView.fireEventShowAllUsers();
         usersView.fireEventOpenUserEditForm(126L);
         editUserView.fireEventUserDeleted(124L);
+        editUserView.fireEventUserChanged("Max",124L, 1);
         usersView.fireEventShowDeletedUsers();
     }
 }
